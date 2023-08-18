1dd1eb4f124301276824e62190283cf458475e8b
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-08-17 21:53:56 -0300

    codigo aula 04

---
+++ vuetify-do-zero/src/views/AboutView.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-form ref="form" @submit.prevent="handleSubmit">
+    <v-text-field 
+      label="Nome" 
+      variant="outlined" 
+      :rules="[v => !!v || 'Nome é obrigatório!']"
+      v-model="nome"
+      required
+    ></v-text-field>
+
+    <v-text-field 
+      label="Email" 
+      variant="outlined" 
+      :rules="[v => !!v || 'Email é obrigatório!']"
+      v-model="email"
+      required
+    ></v-text-field>
+
+    <v-select 
+      label="Estado" 
+      variant="outlined" 
+      :items="estadosList"
+      v-model="estado"
+    >
+
+    </v-select>
+
+    <v-btn variant="tonal" color="green" type="submit">Cadastrar</v-btn>
+    <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
+  </v-form>
+  <v-snackbar
+    v-model="snackbar"
+    :timeout="timeout"
+  >
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn
+        color="red"
+        variant="text"
+        @click="snackbar = false"
+      >
+        Fechar
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      snackbar: false,
+      timeout: 2000,
+      text: "",
+      nome: "",
+      estado: "",
+      email: "",
+      estadosList: [
+        'CE',
+        'SP',
+        'SC',
+        'PR',
+        'RS',
+        'PE'
+      ]
+    }
+  },
+  methods: {
+    async handleSubmit(){
+      const {valid} = await this.$refs.form.validate()
+
+      if(valid){ 
+        this.text = 'Usuário cadastrado com sucesso!'
+        this.snackbar = true
+        this.$refs.form.reset()
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuetify-do-zero/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <v-btn append-icon="mdi-check-bold" color="green" variant="outlined">
+      Button
+    </v-btn>
+    <v-card 
+      title="Titulo do meu card"
+      subtitle="subtítulo do card"
+      variant="tonal"
+      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem molestiae sapiente sed, voluptas, porro aliquid animi dolor sunt similique perferendis totam! Expedita, ullam! Asperiores adipisci exercitationem aperiam ipsa iste ratione?">
+    </v-card>
+  </main>
+</template>
+
+<script>
+</script>
